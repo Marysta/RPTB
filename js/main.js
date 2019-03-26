125d73fb76b406b0b32d664712eb896f397e9ed6
marysta <miss_dryl@yahoo.com>
2019-03-26 12:14:31 +0000

    add new file

--- js/main.js
+++ js/main.js
@@ -250,7 +250,18 @@
 			});
 		}
 	};
+/* ---------------------------------------------- /*
+         * Scroll Animation
+         /* ---------------------------------------------- */
 
+        $('.section-scroll').bind('click', function(e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+        
 	// Owl Carousel
 	var owlCrouselFeatureSlide = function() {
 		var owl = $('.owl-carousel1');
